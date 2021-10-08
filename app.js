5cbb44895cee60e60f01c5d5c9d2aec33fbe77dd
PatKo1010 <Pat19951010>
2021-10-08 15:42:11 +0800

    chore: heroku-init

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require ('express')
 const exphbs = require ('express-handlebars')
-const port = 3000
+const port = process.env.PORT || 3000
 const app = express()
 const bodyParser = require ('body-parser')
 const UrlModel = require ('./models/url')
